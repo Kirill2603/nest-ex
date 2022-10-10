0108596f49d895b4a185bc4acb9a26c778f40a6b
kirill <k.radobolsky@gmail.com>
2022-10-10 11:58:26 +0300

    add post query with DTO

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,15 +1,27 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import { CreateProductDto } from './dto/create-product.dto'
 
 @Controller('products')
 export class ProductsController {
 
   @Get()
-  getAll() {
+  getAll(): string {
     return 'getAll!!'
   }
 
   @Get(':id')
-  getOne(@Param() params) {
-    return 'getOne' + params.id
+  getOne(@Param('id') id: string): string {
+    return 'getOne' + id
   }
+
+  @Post()
+  create(@Body() createProduct: CreateProductDto): string {
+    return `Title: ${createProduct.title}, Price: ${createProduct.price}`
+  }
+
+  // @Delete()
+  // delete() {}
+  //
+  // @Put()
+  // update() {}
 }
